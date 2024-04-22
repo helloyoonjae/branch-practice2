9ac08a3837d44bbbd14a062bbefea3716760f94c
helloyoonjae <helloyoonjae@gmail.com>
2024-04-22 12:27:41 +0900

    feat: Done with fizzbuzz
    resolve #1

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,13 +2,12 @@ import numpy as np
 
 for i in np.arange(1,16,1):
     
-    if i % 3 ==0:
+    if i % 15 ==0:
+        print('fizzbuzz')
+    elif i % 5 ==0:
+        print('buzz')
+
+    elif i % 3 ==0:
         print('fizz')
     else:
         print(i)
-
-    if i % 5 ==0:
-        print('buzz')
-
-    if i % 15 ==0:
-        print('fizzbuzz')
